baff8b7794e4c19616f986fcb4227e2c59d3db6b
paulasmetzker <paulasmetzker@gmail.com>
2016-11-21 11:38:16 +0100

    Update fctgeneral.m
    add comments

--- fctgeneral.m
+++ fctgeneral.m
@@ -1,11 +1,13 @@
 function [f,g]=fctgeneral(x)
+%Première fonction pour tester la robustesse du code
+%Pour x0 = (1;1;...;1)^T
         n=length(x);
         f=0;
         x1 = x(2:n);
         x2 = x(1:n-1);
         i = 2:n;
         f = sum(i* (x1.*2 - x2) .^2);
-
+%
         if(nargout==1)
 	        return;
         end
@@ -14,4 +16,5 @@ function [f,g]=fctgeneral(x)
         
         i = [1:n]';
         g = 8 * i .* vertcat(0,x1) -4*i.*vertcat(0,x2) -4*vertcat(i(2:n),0) .* vertcat(x1,0) + 2* vertcat(i(2:n),0) .* x;
+end
 	
